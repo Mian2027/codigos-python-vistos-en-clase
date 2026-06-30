cc72a47bf17f97f8a7bed4190059b4a605820ff9
rgap <r.guzmanap@gmail.com>
2026-06-30 08:07:37 -0500

    fix: removed bitwise operator exercises

--- 03_operators/practical_exercise/README.md
+++ 03_operators/practical_exercise/README.md
@@ -9,8 +9,7 @@ El programa representa una compra y debe utilizar:
 - operadores aritméticos;
 - operadores de comparación;
 - operadores lógicos;
-- operadores de asignación;
-- operadores bit a bit.
+- operadores de asignación.
 
 ## Datos
 
@@ -33,15 +32,4 @@ Potencia: 8
 ¿La compra está aprobada?: True
 ¿Requiere revisión?: False
 Stock final: 14
-AND bit a bit: 0b100
-OR bit a bit: 0b1101
-XOR bit a bit: 0b1001
-Desplazamiento izquierdo: 0b11000
-Desplazamiento derecho: 0b110
 ```
-
-## Nota sobre operadores bit a bit
-
-Los valores `0b1100` y `0b0101` están escritos en formato binario. La función
-`bin()` permite mostrar el resultado también en formato binario.
-
--- 03_operators/practical_exercise/exercise.py
+++ 03_operators/practical_exercise/exercise.py
@@ -46,71 +46,34 @@ stock =
 # TODO 16: agrega 2 unidades a stock usando +=.
 
 
-# TODO 17: crea el valor binario 0b1100.
-permission_a =
-
-# TODO 18: crea el valor binario 0b0101.
-permission_b =
-
-# TODO 19: aplica AND bit a bit entre permission_a y permission_b.
-bitwise_and =
-
-# TODO 20: aplica OR bit a bit entre permission_a y permission_b.
-bitwise_or =
-
-# TODO 21: aplica XOR bit a bit entre permission_a y permission_b.
-bitwise_xor =
-
-# TODO 22: desplaza permission_a un bit a la izquierda.
-left_shift =
-
-# TODO 23: desplaza permission_a un bit a la derecha.
-right_shift =
-
-# TODO 24: muestra el subtotal.
+# TODO 17: muestra el subtotal.
 
 
-# TODO 25: muestra el vuelto.
+# TODO 18: muestra el vuelto.
 
 
-# TODO 26: muestra las cajas completas.
+# TODO 19: muestra las cajas completas.
 
 
-# TODO 27: muestra las unidades sobrantes.
+# TODO 20: muestra las unidades sobrantes.
 
 
-# TODO 28: muestra la potencia.
+# TODO 21: muestra la potencia.
 
 
-# TODO 29: muestra has_enough_money.
+# TODO 22: muestra has_enough_money.
 
 
-# TODO 30: muestra is_large_purchase.
+# TODO 23: muestra is_large_purchase.
 
 
-# TODO 31: muestra is_exact_payment.
+# TODO 24: muestra is_exact_payment.
 
 
-# TODO 32: muestra is_approved.
+# TODO 25: muestra is_approved.
 
 
-# TODO 33: muestra requires_review.
+# TODO 26: muestra requires_review.
 
 
-# TODO 34: muestra el stock final.
-
-
-# TODO 35: muestra bitwise_and usando bin().
-
-
-# TODO 36: muestra bitwise_or usando bin().
-
-
-# TODO 37: muestra bitwise_xor usando bin().
-
-
-# TODO 38: muestra left_shift usando bin().
-
-
-# TODO 39: muestra right_shift usando bin().
-
+# TODO 27: muestra el stock final.
